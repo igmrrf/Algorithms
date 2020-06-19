05b7ec8b6a2839e853f717442a551813e05482cb
igmrrf <francis.igbiriki.fi@gmail.com>
2020-06-19 19:48:21 +0100

    Inital Commit

---
+++ Java/Scala.scala
@@ -0,0 +1,11 @@
+var a = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var b = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var c = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+a.forEach{
+    d in b.forEach{
+        e in c.forEach{
+            f in print(d + e + f)
+        }
+    }
+ }
